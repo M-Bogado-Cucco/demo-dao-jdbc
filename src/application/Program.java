352af5b72f3a8eedcadbbb327cfece4b1087ed89
Marcelo BC <celombc@gmail.com>
2021-09-16 18:25:00 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		
+		
+		
 		System.out.println(obj);
 	}
 
